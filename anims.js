1bc18672281dc0000f24c04336bcc162abbfbe1c
Mr-Forder <66869833+Mr-Forder@users.noreply.github.com>
2021-04-13 12:12:17 +0100

    added basic gsap anims

---
+++ anims.js
@@ -0,0 +1,35 @@
+gsap.fromTo(".library-panel", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".library-panel", { y: "1rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".generate-panel", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".generate-panel", { y: "1rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".save-panel", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".save-panel", { y: "1rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".logo", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".logo", { y: "-5rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".nav-links", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".nav-links", { y: "-5rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".hamburger", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".hamburger", { x: "5rem" }, { x: "0rem", duration: 1 });
+
+gsap.fromTo(".about-content", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".about-content", { y: "1rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".about-content h1", { opacity: 0 }, { opacity: 1, duration: 1 });
+gsap.fromTo(".about-content h1", { y: "-1rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".fwrap", { opacity: 0 }, { opacity: 1, duration: 3 });
+gsap.fromTo(".fwrap", { y: "1rem" }, { y: "0rem", duration: 1 });
+
+gsap.fromTo(".intro", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+TweenMax.to(".big-logo", 0.5, {
+  scale: 1.2,
+  repeat: -1,
+  ease: Bounce.easeOut,
+  yoyoEase: Power3.easeOut,
+});
